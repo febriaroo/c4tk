1f9754aef23195b0ab22a8aea1921cc4396c60c8
Febria Roosita Dwi <froositadwi@cermati-dev>
2017-10-21 16:02:21 +0700

    editing firebase

--- files/controller.js
+++ files/controller.js
@@ -14,6 +14,7 @@ $(page)
         }, function (tryAgain) {
           if (tryAgain) {
             submitUserToFirebase();
+            selectFromFirebase()
             App.load('thankyou')
           }
         });
@@ -89,4 +90,36 @@ function submitUserToFirebase(){
   console.log(data);
 
   ref.set(dataToSend);
+}
+
+function settingGoogleMaps(){
+  var mapcanvas = document.createElement('div');
+  mapcanvas.id = 'mapcanvas';
+  mapcanvas.style.height = '400px';
+  mapcanvas.style.width = '560px';
+
+  document.querySelector('article').appendChild(mapcanvas);
+
+  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var myOptions = {
+    zoom: 15,
+    center: latlng,
+    mapTypeControl: false,
+    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
+
+  var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title:"You are here! (at least within a "+position.coords.accuracy+" meter radius)"
+  });
+}
+
+function selectFromFirebase(){
+  var ref = firebase.database().ref('products');
+  ref.orderByChild("height").startAt(3).on("child_added", function(snapshot) {
+    console.log(snapshot.key())
+  });
 }
