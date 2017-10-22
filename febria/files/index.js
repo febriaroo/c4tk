21812e927dfe26ebdab6540a256368f518957c13
Roger Stach <rogerdanistach@gmail.com>
2017-10-22 11:39:52 +0700

    new shit just got made

--- febria/files/index.js
+++ febria/files/index.js
@@ -1,3 +1,15 @@
+function initFirebase() {
+    var config = {
+      apiKey: "AIzaSyBprCTN37vforliHYlbS4QZPdZp3xX-PO0",
+      authDomain: "jakat-id.firebaseapp.com",
+      databaseURL: "https://jakat-id.firebaseio.com",
+      projectId: "jakat-id",
+      storageBucket: "jakat-id.appspot.com",
+      messagingSenderId: "1059928732908"
+    };
+    firebase.initializeApp(config);
+}
+
 function changeBarang(category) {
   console.log('Current Category : ' + category)
   
@@ -18,4 +30,43 @@ function changeBarang(category) {
 	  	$(".form-pakaian").addClass("is-hidden");
   		break;
   }
+}
+
+function addItem() {
+  initFirebase();
+
+  var rootRef = firebase.database().ref();
+  var ref = rootRef.child('donasi').push(),
+	  unique = ref.key;
+  var storageRef = firebase.storage().ref(unique);
+
+  var tipe_barang = $('#tipe_barang').val(),
+      merek = $('#merek').val(),
+      tipe_perangkat = $('#tipe_perangkat').val(),
+      file = $('#image').prop('files')[0],
+      kondisi_raw = document.getElementsByName('kondisi'),
+      kondisi;
+  
+  for (var i = 0, n = kondisi_raw.length ; i < n; i++) {
+      if (kondisi_raw[i].checked) {
+          kondisi = kondisi_raw[i].value;
+          break;
+      }
+  }
+
+  var datas = {
+  	tipe_barang: tipe_barang,
+  	merek: merek,
+  	tipe_perangkat: tipe_perangkat,
+  	image: unique,
+  	kondisi: kondisi
+  }
+
+  // console.log(tipe_barang, merek, tipe_perangkat, kondisi, file)
+  console.log('masukin data')
+  storageRef.put(file)
+  ref.set(datas)
+  console.log('sukses')
+
+  window.location.href = 'detailJakatur.html/?key=' + unique;
 }
